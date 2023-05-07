41aecce8e0656fdc6a24a152956b1f69eae57403
toshi0607 <s.toshi0607@gmail.com>
2023-05-07 21:33:36 +0900

    Create and query user

--- ent/entdemo/main.go
+++ ent/entdemo/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"entdemo/ent"
+	"entdemo/ent/user"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -19,4 +21,45 @@ func main() {
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+
+	ctx := context.Background()
+
+	user, err := CreateUser(ctx, client)
+	if err != nil {
+		log.Fatalf("failed create user: %v", err)
+	}
+	fmt.Printf("user name: %s\n", user.Name)
+
+	gotUser, err := QueryUser(ctx, client)
+	if err != nil {
+		log.Fatalf("failed query user: %v", err)
+	}
+	fmt.Printf("user name: %s\n", gotUser.Name)
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.Name("a8m")).
+		// `Only` fails if no user found,
+		// or more than 1 user returned.
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %w", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
 }
